6cf4f7cd5fdbb6f1a8a380508afe6c17a3804c2d
DeadSakura <taoda001@gmail.com>
2014-02-27 01:06:43 -0600

    More like old New Tab page

--- scripts/main.js
+++ scripts/main.js
@@ -3,51 +3,67 @@
 document.addEventListener('DOMContentLoaded', function () {
     chrome.topSites.get(getMostVisited);
     chrome.management.getAll(getAllApps);
-    createComponent("#tabs", "tab");
+    createComponent("#tabs", function(filter) {$(filter).tabs({heightStyle: "fill"});});
 });
             
 function getMostVisited(data) {
-    for (var i = data.length - 1; i >= 0; i--) {
-        $("#mostVisited").append(data[i].url+"<br>");
-        $("#mostVisited").append(data[i].title+"<br>");
+    var topSiteItem = ' <li class="topSite">\
+                        <a href="{0}" >\
+                        <img src="chrome://favicon/{0}" />\
+                        {1}\
+                        </a>\
+                        </li>'
+    $("#mostVisited").append('<ul id="menu">')                  
+    for (var i = 0; i < data.length - 1; i++) {
+        $("#menu").append(topSiteItem.format(data[i].url, data[i].title));
     }
-    console.log("Top Site Count:" + data.length);
+    $("#mostVisited").append('</ul>')
+    createComponent("#menu", function(filter){
+        $(filter).menu();
+    });
     
 }
 
 function getAllApps(data) {
+    // String for apps.
+    var launcherItem = '<li class="launcher" appId="{0}">\
+                        <div>\
+                        <img src="{2}" width="128" height="128" />\
+                        </div>\
+                        <span>\
+                        {1}\
+                        </span>\
+                        </li>'
+    console.log("Installed App Count:" + data.length);
     for (var i = data.length - 1; i >= 0; i--) {
+        console.log("Found App: " + data[i].name + " type:" + data[i].type);
         if (data[i].type == 'theme' || 
             data[i].type == 'extension' ) {
             continue;
         };
-        $("#installedApps").append("<div class='launcher' appid='" + data[i].id + "'><img src='" + data[i].icons[data[i].icons.length - 1].url + "'><br>" + data[i].name + "</div>");
-        console.log("App" + data[i].name + "has" + data[i].icons.length + "icons");
-        console.log("Sizes availiable:");
-        for (var j = data[i].icons.length - 1; j >= 0; j--) {
-            console.log(data[i].icons[j].size);
-        };
+        $("#appContainer")
+            .append(launcherItem.format(data[i].id, 
+                                        data[i].name ,
+                                        data[i].icons[data[i].icons.length - 1].url));
     }
-    createComponent(".launcher", "button");
-    console.log("Installed App Count:" + data.length);
+
+    $("#appContainer").sortable();
+    $("#appContainer").disableSelection();
+    createComponent(".launcher", function(filter){
+        $(filter).each(function(index, element){
+            $(element)
+            .button()
+            .click(function(){
+                chrome.management.launchApp($(this).attr("appId"));
+            })
+
+        })
+    });
+    
 }
 
-function createComponent(filter, type){
-    $(function() {
-        switch(type){
-            case 'tab':
-                $( filter ).tabs();
-                break;
-            case 'button':
-                $( filter )
-                    .button()
-                    .click(function(){
-                        chrome.management.launchApp($( this ).attr('appid'));
-                    });
-
-                break;
-        }
-    });
+function createComponent(filter, packFunction){
+    packFunction(filter);
 }
 
 function getAndSaveApplist(data) {
